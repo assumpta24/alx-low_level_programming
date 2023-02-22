bf522ad5b2e002407f60f63a695a03d175774f3e
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-02-22 04:04:45 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,30 +2,30 @@
 
 /**
  * jack_bauer - Prints every minute of the day of jack bauer
- *
+ * starting from 00:00 to 23:59
  */
 
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int i, j;
 
-	for (a = 0; a <= 2; a++)
+	i = 0;
+
+	while (i < 24)
 	{
-	for (b = 0; b <= 9; b++)
-	{
-	for (c = 0; c <= 5; c++)
-	{
-	for (d = 0; d <= 9; d++)
-	{
-	_putchar(a + '0');
-	_putchar(b + '0');
-	_putchar(58);
-	_putchar(c + '0');
-	_putchar(d + '0');
-	_putchar('\n');
-	}
-	}
-	}
+		j = 0;
+
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchard('\n');
+			j++;
+		}
+		i++;
 	}
 
 }
