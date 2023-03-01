7ca1f39300fdd495cb8adabac8579d20cd24307b
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-03-01 13:21:23 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,36 +1,33 @@
 #include "main.h"
 
 /**
- * _strncat - joins two strings with n bytes
- * function- main
- * description: the starting of while loop
- * @dest: copy to
- * @src: copy from
- * @n: number of char to be copied
- * Return: dest
- */
-char *_strncat(char *dest, char *src, int n);
-{
-int i;
-int j;
-i = 0;
+ * _strncat - a function that concatenates two strings.
+ *
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * @n: most number of bytes from @src
+ * Written by Assumpta
+ * Return: @dest
+*/
 
-int main(void)
+char *_strncat(char *dest, char *src, int n)
 {
-while (dest[i] != '\0')
-{
-i++;
+	int c, i;
 
-}
-j = 0;
+	c = 0;
 
-while (j < n && src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest = '\0';
-return (dest);
-}
+	/*find size of dest array*/
+	while (dest[c])
+		c++;
+
+	/**
+	 * src does not need to be null terminated
+	 * if it contains n or more bytes
+	*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	/*null terminate dest*/
+	dest[c + i] = '\0';
+
+	return (dest);
 }
