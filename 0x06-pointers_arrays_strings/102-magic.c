0d764b9ee77a5661840089313931a280bc18425b
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-03-02 12:36:05 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
+ *
  * main - demo program
  *
- * Return: Always 0
+ * Return: 0 if successful else 1 on error
  */
 int main(void)
 {
@@ -13,14 +14,14 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
 p[5] = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
