b2726d5454116aa25362588c51451938a6e7fbeb
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-03-13 15:42:24 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,35 +7,25 @@
  *
  * Return: number of words
  */
-
 int count_word(char *s)
 {
-		int flag, c, w;
+	int flag, c, w;
 
+	flag = 0;
+	w = 0;
 
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
 			flag = 0;
+		    else if (flag == 0)
+		    {
+                     flag = 1;
+	             w++;
+		    }
+	}
 
-				w = 0;
-
-
-					for (c = 0; s[c] != '\0'; c++)
-
-							{
-
-										if (s[c] == ' ')
-
-														flag = 0;
-
-												else if (flag == 0)
-
-															{
-																			flag = 1;
-																						w++;
-
-																								}
-
-													}
-						return (w);
+	return (w);
 }
 /**
  * **strtow - splits a string into words
@@ -51,16 +41,12 @@ char **strtow(char *str)
 
 	while (*(str + len))
 		len++;
-
 	words = count_word(str);
-
 	if (words == 0)
 		return (NULL);
 
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
-
 	if (matrix == NULL)
-
 		return (NULL);
 
 	for (i = 0; i <= len; i++)
@@ -70,24 +56,19 @@ char **strtow(char *str)
 			if (c)
 			{
 				end = i;
-
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
-
 				if (tmp == NULL)
 					return (NULL);
 
 				while (start < end)
-					*tmp++ = str[start++];
-
+				*tmp++ = str[start++];
 				*tmp = '\0';
 				matrix[k] = tmp - c;
 				k++;
-
 				c = 0;
 			}
 		}
 		else if (c++ == 0)
-
 			start = i;
 	}
 	matrix[k] = NULL;
