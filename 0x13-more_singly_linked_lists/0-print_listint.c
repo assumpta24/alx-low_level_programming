2afba40843bd67586a7b0070b5f6be5a4dfb0bdd
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-04-04 15:10:01 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,23 +5,19 @@
  * print_listint - function that prints all elements in linked list
  * @h: poiner to be listed
  *
- * Return: returns the number of nodes
+ * Return: the number of nodes
  *
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t nbr = 0;
 
-	*h = Null;
 
-	if (h == Null)
-		printf("empty linked list:");
-
-	while (h != Null)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		nbr++;
+		h = h->next;
 	}
 	return (nbr);
 }
