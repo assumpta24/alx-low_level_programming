2746fe4fc987fed1933c92634b71b526325b31db
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-04-04 14:29:59 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 1;
+	size_t nodes = 0;
 
-	while (h)
+	while (h != Null)
 	{
 		printf("%d\n", h->n);
-		nodes++;
+
 		h = h->next;
 	}
 	return (nodes);
