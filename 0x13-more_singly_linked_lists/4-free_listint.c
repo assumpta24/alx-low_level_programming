2cb03e9c34f804d5e092bbc807d22165e45793af
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-04-05 12:05:02 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,13 +8,15 @@
  *
  * Return: nothing
  */
-void free_listint(listint_t *head);
+void free_listint(listint_t *head)
 {
 	listint_t *ptr;
 
-	if (head != NULL)
+	ptr = head;
+
+	while (ptr != NULL)
 	{
-		ptr = head;
+
 		head = head->next;
 
 		free(ptr);
