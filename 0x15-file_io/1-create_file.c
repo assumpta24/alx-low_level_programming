4afda7cc6831d2b241f3539a6a3b288f10ad5138
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-04-10 14:35:22 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	fa = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
 
-	while (fa == -1)
+	if (fa == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
