e351503768df27594820daf46bb03b8fe3e9185c
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-05-04 13:24:20 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to flip
+ * @n: arguments represent a number to be compared
+ * @m: argument represents a number to be compared
+ *
+ * Return: returns a number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m;
+	unsigned long int total = 0;
+
+	while (xor > 0)
+	{
+		total += xor & 1;
+		xor >>= 1;
+	}
+	return (total);
+}
