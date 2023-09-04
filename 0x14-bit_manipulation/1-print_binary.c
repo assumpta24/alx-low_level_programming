fed29150e43eecaae2eaf2ca632dce83c4092c13
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-09-04 12:02:02 -0700

    modified 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,10 +7,17 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if ( n >> 0)
 	{
-		print_binary(n >> 1);
-		putchar((n & 1) ? '1' : '0');
+		if (n >> 1)
+
+			print_binary(n >> 1);
+		putchar((n & 1) +  '0');
 	}
+	else
+	{
+		putchar('0');
+	}
+
 }
 
